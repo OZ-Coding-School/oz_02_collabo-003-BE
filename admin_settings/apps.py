fcbc1e66b5e73dd2979cdf9ad2a1fb466a970ebe
Hyunminmax <hyunmin.shin81@gmail.com>
2024-04-27 18:08:33 +0900

    conf:DB Connect, startapp:admin_settings, admins, luck_messages, gpt_prompt

---
+++ admin_settings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminSettingsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "admin_settings"
---
+++ luck_messages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LuckMessagesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "luck_messages"
