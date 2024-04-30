9a42c29626a0e53752402b960f5b0f0a5f0563ef
BE-02-KMJ <mjk5912@naver.com>
2024-04-30 16:56:38 +0900

    views.py change

--- gpt_prompts/views.py
+++ gpt_prompts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import PromptEachSerializer, PromptUpdateSerializer, PromptAllSerializer
+from .models import GptPrompt
 
 # Create your views here.
+class GptPromptEach(APIView):
+    serializer_class = PromptEachSerializer
+    def get(self, request):
+        try:
+            # 카테고리별 가장 마지막 gpt_id 보여주기
+            pass
+        except:
+            pass
+
+
+class GptPromptUpdate(APIView):
+    pass
+
+class GptPromptAll(APIView):
+    pass
