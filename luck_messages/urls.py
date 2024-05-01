59dd297f47e3b4fcb242fffd543b17ae3921012e
Hyunminmax <hyunmin.shin81@gmail.com>
2024-05-01 17:56:57 +0900

    Feat: feat_BE-LUCK201

---
+++ luck_messages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import findTodayZodiacMessages
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('zodiac_all/<str:attribute1>', findTodayZodiacMessages.as_view(), name='findTodayZodiacMessages')
+
+]
--- luck_messages/views.py
+++ luck_messages/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from datetime import datetime
+from rest_framework import views, status
+from rest_framework.response import Response
+from .serializers import *
 
-# Create your views here.
+
+class findTodayZodiacMessages(views.APIView):
+    serializer_class = zodiacSerializer
+    def get(self, request, attribute1):
+        now = datetime.now()
+        date = now.strftime("%Y%m%d")
+        reqCategory = "zodiac"
+        messages = LuckMessage.objects.filter(luck_date=date, category=reqCategory, attribute1=attribute1)
+        serializer = zodiacSerializer(messages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
