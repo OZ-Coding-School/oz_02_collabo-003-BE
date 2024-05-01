794716fb21d726e9264c239f51a01cd1abda0393
Hyunminmax <hyunmin.shin81@gmail.com>
2024-05-01 20:51:43 +0900

    Refactor serializers update

--- luck_messages/serializers.py
+++ luck_messages/serializers.py
@@ -5,4 +5,4 @@ class todaySerializer(ModelSerializer):
     class Meta:
         model = LuckMessage
         # fields = '__all__'  # 모든 필드 포함
-        fields = ('luck_date', 'category', 'attribute2', 'luck_msg')
+        fields = ('msg_id', 'luck_date', 'category', 'attribute2', 'luck_msg')
