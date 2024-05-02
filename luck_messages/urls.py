a306f4802ea4a4a1a5bbbb22024ac4329f625baa
Hyunminmax <hyunmin.shin81@gmail.com>
2024-05-02 18:43:44 +0900

    build feat_BE-LUCK201 + feat_BE-LUCK301

--- luck_messages/urls.py
+++ luck_messages/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from .views import findTodayStarMessages
+from .views import findTodayStarMessages, findTodayZodiacMessages
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('zodiac_all/<str:attribute1>', findTodayZodiacMessages.as_view(), name='findTodayZodiacMessages'),
     path('star_all/', findTodayStarMessages.as_view(), name='findTodayStarMessages')
 
 ]
--- luck_messages/views.py
+++ luck_messages/views.py
@@ -5,6 +5,17 @@ from rest_framework.response import Response
 from .serializers import *
 
 
+class findTodayZodiacMessages(views.APIView):
+    serializer_class = zodiacSerializer
+    def get(self, request, attribute1):
+        now = datetime.now()
+        date = now.strftime("%Y%m%d")
+        reqCategory = "zodiac"
+        messages = LuckMessage.objects.filter(luck_date=date, category=reqCategory, attribute1=attribute1)
+        serializer = zodiacSerializer(messages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 class findTodayStarMessages(views.APIView):
     serializer_class = starSerializer
     def get(self, request):
