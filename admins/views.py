de41a6697e4e771162261fce6e1b6e02a7a9fb17
Hyunminmax <hyunmin.shin81@gmail.com>
2024-05-03 14:10:03 +0900

    test

--- admins/views.py
+++ admins/views.py
@@ -3,8 +3,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
 from luck_messages.models import LuckMessage
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.exceptions import ParseError
 
-# api/v1/admin/msg/<int:msg_id>/update/
+
+# api/v1/admin/signup/
+class AdminUsers(APIView):
+    serializer_class = AdminSignupSerializer
+    def post(self, request):
+
+        password = request.data.get('user_pw')
+        serializer = AdminSignupSerializer(data=request.data)
+
+        try:
+            validate_password(password)
+        except:
+            raise ParseError('Password is invalid.')
+
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ParseError(serializer.errors)
+
+
+# api/v1/admin/msg/
 class EditLuckMessage(APIView):
     '''
     프론트에서 msg_id를 받아 luck_messages의 모델에서 해당 id를 찾아 내용 수정
