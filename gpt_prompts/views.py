773b4886b47ebc3535d1cc6d1e37dfe523b7a9f3
BE-02-KMJ <mjk5912@naver.com>
2024-06-03 20:27:02 +0900

    Fix: Prompt 메세지 조회 및 생성 코드 하나로 합치기

--- gpt_prompts/views.py
+++ gpt_prompts/views.py
@@ -13,24 +13,24 @@ from .serializers import *
 from .models import GptPrompt
 
 
-# 오늘의 한마디 프롬프트
-# api/v1/prompt/today
-class PromptToday(APIView):
+# 각 카테고리별 프롬프트 조회 및 생성
+# api/v1/prompt/{category}/
+class PromptIndividual(APIView):
     '''
-    BE-GPT101(GET): 오늘의 한마디에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드\n
-    BE-GPT102(POST): 오늘의 한마디에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장
+    BE-GPT101, 201, 301, 401(GET): 카테고리별 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드\n
+    BE-GPT102, 202, 302, 402(POST): 카테고리별 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장
     '''
-    serializer_class = PromptTodaySerializer
+    serializer_class = PromptSerializer
 
+    # 프롬프트 메세지 최신 1개 조회
     @extend_schema(tags=['PromptMsg'],
-                    description="BE-GPT101(GET): 오늘의 한마디에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드"
+                    description="BE-GPT101, 201, 301, 401(GET): 카테고리별 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드"
     )
-    def get(self, request):
+    def get(self, request, category):
         # 업데이트하는 방식 X, 프롬프트 메세지 이름 사용 X
         try:
-            category = "today"
             latest_today = GptPrompt.objects.filter(category=category).order_by('-gpt_id').first()
-            serializer = PromptTodaySerializer(latest_today)
+            serializer = PromptSerializer(latest_today)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except GptPrompt.DoesNotExist:
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
@@ -39,21 +39,39 @@ class PromptToday(APIView):
     @extend_schema(tags=['PromptMsg'],
         examples=[
             OpenApiExample(
-                'Example',
-                value={'prompt_msg' : "오늘의 한마디를 총 3개를 작성할거야. 아침에 하루를 시작하는 사람들이 이 글을보고 힘이나고 위로를 받았으면 해. 작성 방법은 예시를 참고해줘. 예시 '꽃비 내리는 날 설레이는 봄이에요.🌟 꽃 향기처럼 부드럽고 향기로운 하루 보내시길 바래요.🌈 당신의 편에 서서 응원할게요!💪' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시  '오늘', '오늘은' 이라는 단어는 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 문장 가운데마다 이모티콘을 적절히 2개 이상 4개 미만으로 넣어서 작성해줘. 내용 길이를  45자 이상 50자 미만으로 작성해주고, 2문장으로 작성해줘."},
+                'Example - category: today',
+                value={'prompt_msg' : "오늘의 한마디를 총 3개를 작성할거야. 아침에 하루를 시작하는 사람들이 이 글을보고 힘이나고 위로를 받았으면 해. 작성 방법은 예시를 참고해줘. 예시 '꽃비 내리는 날 설레이는 봄이에요.🌟 꽃 향기처럼 부드럽고 향기로운 하루 보내시길 바래요.🌈 당신의 편에 서서 응원할게요!💪' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시  '오늘', '오늘은' 이라는 단어는 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 문장 가운데마다 이모티콘을 적절히 2개 이상 4개 미만으로 넣어서 작성해줘. 내용 길이를  45자 이상 50자 미만으로 작성해주고, 2문장으로 작성해줘."
+                },
+                request_only=True,  # 요청 본문에서만 예시 사용
+            ),
+            OpenApiExample(
+                'Example - category: zodiac',
+                value={'prompt_msg' : "띠별 운세를 작성할꺼야. 작성해야하는 대상자는 1960년 부터 2007년에 태어난 사람이야. 가장 큰 제목은 띠이고, 세부항목은 태어난 연도이고 해당되는 연도를 각각 나눠서 작성해야해. 작성 방법은 예시를 참고해줘. 예시: '원숭이 1968 직장에서 긍정적인 변화가 기다리고 있습니다. 새로운 프로젝트나 업무가 기회가 될 수 있습니다. 적극적인 태도가 중요합니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시  '오늘 ~년생'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해줘. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고, 3문장으로 작성해줘."
+                },
+                request_only=True,  # 요청 본문에서만 예시 사용
+            ),
+            OpenApiExample(
+                'Example - category: star',
+                value={'prompt_msg' : "별자리별 운세를 작성할꺼야. '물병자리 (01/20~02/18)', '물고기자리 (02/19~03/20)', '양자리 (03/21~04/19)', '황소자리 (04/20~05/20)', '쌍둥이자리 (05/21~06/21)', '게자리 (06/22~07/22)', '사자자리 (07/23~08/22)', '처녀자리 (08/23~09/22)', '천칭자리 (09/23~10/22)', '전갈자리 (10/23~11/21)', '궁수자리 (11/22~12/21)', '염소자리 (12/22~01/19)' 총 12개의 별자리이고 작성방법은 예시를 참고해줘. 예시:'물병자리 (01/20~02/18) 오늘은 어디를 가서도 당신의 밥그릇은 챙길 수 있는 날입니다. 되도록 마음을 크게 먹는 것이 좋습니다. 쪼잔 하다는 소리를 듣지 않도록 조심하세요. 당신의 마음 수양이 제대로 이루어질수록 행운이 따릅니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시 '오늘 ~별자리'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고, 3문장으로 작성해줘."
+                },
+                request_only=True,  # 요청 본문에서만 예시 사용
+            ),
+            OpenApiExample(
+                'Example - category: mbti',
+                value={'prompt_msg' : "작성해야하는 MBTI 유형이야. ISTJ, ISFJ, INFJ, INTJ, ISTP, ISFP, INFP, INTP, ESTP, ESFP, ENFP, ENTP, ESTJ, ESFJ, ENFJ, ENTJ 총 16개의 MBTI 각 유형별로 작성해줘. 작성 방법은 예시를 참고해줘. 예시'ISTJ 오늘은 당신에게 청정한 감성과 정확성이 빛나는 하루가 될 것입니다. 일에 대한 책임감을 가지고 차분하게 일 처리를 하면 좋은 결과를 얻을 수 있을 것입니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시 '오늘은'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고,  3문장으로 작성해."
+                },
                 request_only=True,  # 요청 본문에서만 예시 사용
             )
         ],
         description="BE-GPT102(POST): 오늘의 한마디에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장"
     )
-    def post(self, request):
+    def post(self, request, category):
         now = datetime.now()
         today = now.strftime('%Y%m%d')
         admins_id = 1
-        serializer = PromptTodaySerializer(data=request.data, context={'admins_id': admins_id})
+        serializer = PromptSerializer(data=request.data, context={'admins_id': admins_id})
 
         if serializer.is_valid():
-            category = 'today'
             prompt_msg_name = today
             create_date = today
             # last = now + timedelta(days=7)
@@ -68,165 +86,8 @@ class PromptToday(APIView):
     def post(self, request):
         return True
 
-# 띠별 운세 프롬프트
-# api/v1/prompt/zodiac
-class PromptZodiac(APIView):
-    '''
-    BE-GPT201(GET): 띠별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드\n
-    BE-GPT202(POST): 띠별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장
-    '''
-    serializer_class = PromptZodiacSerializer
 
-    @extend_schema(tags=['PromptMsg'],
-                    description="BE-GPT201(GET): 띠별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드"
-    )
-    def get(self, request):
-    # 업데이트하는 방식 X, 프롬프트 메세지 이름 사용 X
-        try:
-            category = "zodiac"
-            latest_zodiac = GptPrompt.objects.filter(category=category).order_by('-gpt_id').first()
-            serializer = PromptZodiacSerializer(latest_zodiac)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except GptPrompt.DoesNotExist:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    # 프롬프트 메세지 수정 - 추가하는 방식
-    @extend_schema(tags=['PromptMsg'],
-        examples=[
-            OpenApiExample(
-                'Example',
-                value={'prompt_msg' : "띠별 운세를 작성할꺼야. 작성해야하는 대상자는 1960년 부터 2007년에 태어난 사람이야. 가장 큰 제목은 띠이고, 세부항목은 태어난 연도이고 해당되는 연도를 각각 나눠서 작성해야해. 작성 방법은 예시를 참고해줘. 예시: '원숭이 1968 직장에서 긍정적인 변화가 기다리고 있습니다. 새로운 프로젝트나 업무가 기회가 될 수 있습니다. 적극적인 태도가 중요합니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시  '오늘 ~년생'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해줘. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고, 3문장으로 작성해줘."
-                },
-                request_only=True,  # 요청 본문에서만 예시 사용
-            )
-        ],
-        description="BE-GPT202(POST): 띠별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장"
-    )
-    def post(self, request):
-        now = datetime.now()
-        today = now.strftime('%Y%m%d')
-        admins_id = 1
-        serializer = PromptZodiacSerializer(data=request.data, context={'admins_id': admins_id})
-
-        if serializer.is_valid():
-            category = 'zodiac'
-            prompt_msg_name = today
-            create_date = today
-            last = now + timedelta(days=7)
-            last_date = last.strftime('%Y%m%d')
-
-            serializer.save(category=category, prompt_msg_name=prompt_msg_name,
-                            create_date=create_date, last_date=last_date, admins_id=admins_id)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-
-# 별자리별 운세 프롬프트
-# api/v1/prompt/star
-class PromptStar(APIView):
-    '''
-    BE-GPT301(GET): 별자리별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드\n
-    BE-GPT302(POST): 별자리별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장
-    '''
-    serializer_class = PromptStarSerializer
-
-    @extend_schema(tags=['PromptMsg'],
-        description="BE-GPT301(GET): 별자리별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드"
-    )
-    def get(self, request):
-        # 업데이트하는 방식 X, 프롬프트 메세지 이름 사용 X
-        # api/v1/prompt/star
-        try:
-            category = "star"
-            latest_star = GptPrompt.objects.filter(category=category).order_by('-gpt_id').first()
-            serializer = PromptStarSerializer(latest_star)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except GptPrompt.DoesNotExist:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    # 프롬프트 메세지 수정 - 추가하는 방식
-    @extend_schema(tags=['PromptMsg'],
-        examples=[
-            OpenApiExample(
-                'Example',
-                value={'prompt_msg' : "별자리별 운세를 작성할꺼야. '물병자리 (01/20~02/18)', '물고기자리 (02/19~03/20)', '양자리 (03/21~04/19)', '황소자리 (04/20~05/20)', '쌍둥이자리 (05/21~06/20)', '게자리 (06/21~07/22)', '사자자리 (07/23~08/22)', '처녀자리 (08/23~09/22)', '천칭자리 (09/23~10/22)', '전갈자리 (10/23~11/21)', '궁수자리 (11/22~12/21)', '염소자리 (12/22~01/19)' 총 12개의 별자리이고 작성방법은 예시를 참고해줘. 예시:'물병자리 (01/20~02/18) 오늘은 어디를 가서도 당신의 밥그릇은 챙길 수 있는 날입니다. 되도록 마음을 크게 먹는 것이 좋습니다. 쪼잔 하다는 소리를 듣지 않도록 조심하세요. 당신의 마음 수양이 제대로 이루어질수록 행운이 따릅니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시 '오늘 ~별자리'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고, 3문장으로 작성해줘."
-                },
-                request_only=True,  # 요청 본문에서만 예시 사용
-            )
-        ],
-        description="BE-GPT302(POST): 별자리별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장"
-    )
-    def post(self, request):
-        now = datetime.now()
-        today = now.strftime('%Y%m%d')
-        admins_id = 1
-        serializer = PromptStarSerializer(data=request.data, context={'admins_id': admins_id})
-
-        if serializer.is_valid():
-            category ='star'
-            prompt_msg_name = today
-            create_date = today
-            last = now + timedelta(days=7)
-            last_date = last.strftime('%Y%m%d')
-
-            serializer.save(category=category, prompt_msg_name=prompt_msg_name,
-                            create_date=create_date, last_date=last_date, admins_id=admins_id)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# MBTI별 운세 프롬프트
-# api/v1/prompt/mbti
-class PromptMbti(APIView):
-    '''
-    BE-GPT401(GET): MBTI별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드\n
-    BE-GPT402(POST): MBTI별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장
-    '''
-    serializer_class = PromptMbtiSerializer
-
-    @extend_schema(tags=['PromptMsg'],
-                    description="BE-GPT401(GET): MBTI별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 로드"
-    )
-    def get(self, request):
-        # 업데이트하는 방식 X, 프롬프트 메세지 이름 사용 X
-        try:
-            category = "mbti"
-            latest_mbti = GptPrompt.objects.filter(category=category).order_by('-gpt_id').first()
-            serializer = PromptMbtiSerializer(latest_mbti)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except GptPrompt.DoesNotExist:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    # 프롬프트 메세지 수정 - 추가하는 방식
-    @extend_schema(tags=['PromptMsg'],
-        examples=[
-            OpenApiExample(
-                'Example',
-                value={'prompt_msg' : "작성해야하는 MBTI 유형이야. ISTJ, ISFJ, INFJ, INTJ, ISTP, ISFP, INFP, INTP, ESTP, ESFP, ENFP, ENTP, ESTJ, ESFJ, ENFJ, ENTJ 총 16개의 MBTI 각 유형별로 작성해줘. 작성 방법은 예시를 참고해줘. 예시'ISTJ 오늘은 당신에게 청정한 감성과 정확성이 빛나는 하루가 될 것입니다. 일에 대한 책임감을 가지고 차분하게 일 처리를 하면 좋은 결과를 얻을 수 있을 것입니다.' 과하게 부정적인 내용, 성적인 내용, 추상적인 내용은 피해줘. 내용 작성 시 '오늘은'이라는 말은 제외하고 어투는 너무 딱딱하지 않고 부드러우면서도 반말은 사용하지 말고 존댓말을 사용해. 각각의 운세 내용 길이를 60자 이상 65자 미만으로 충분히 길게 작성해주고,  3문장으로 작성해."
-                },
-                request_only=True,  # 요청 본문에서만 예시 사용
-            )
-        ],
-        description="BE-GPT402(POST): MBTI별 운세에 사용되는 최신(마지막 gpt_id) 프롬프트 메세지 저장"
-    )
-    def post(self, request):
-        now = datetime.now()
-        today = now.strftime('%Y%m%d')
-        admins_id = 1
-        serializer = PromptMbtiSerializer(data=request.data, context={'admins_id': admins_id})
-
-        if serializer.is_valid():
-            category ='MBTI'
-            prompt_msg_name = today
-            create_date = today
-            last = now + timedelta(days=7)
-            last_date = last.strftime('%Y%m%d')
-
-            serializer.save(category=category, prompt_msg_name=prompt_msg_name,
-                            create_date=create_date, last_date=last_date, admins_id=admins_id)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# 각 유형별 운세 프롬프트 히스토리
+# 각 카테고리별 운세 프롬프트 히스토리
 # api/v1/prompt/<str:category>/history
 class PromptHistory(APIView):
     '''
