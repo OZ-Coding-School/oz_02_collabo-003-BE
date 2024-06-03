f1dafecbcfd78d44dc84d95f24d775e9dcfcc97d
Hyunminmax <hyunmin.shin81@gmail.com>
2024-06-03 16:31:58 +0900

    ãRefactor: DB schema update

--- admin_settings/models.py
+++ admin_settings/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class AdminSetting(models.Model):
     adms_id = models.AutoField(primary_key=True)
     push_time = models.CharField(max_length=4, blank=True, null=True)
-    # update_date = models.DateTimeField(blank=True, null=True)
+    term_date = models.CharField(max_length=4, blank=True, null=True)
+    term_time = models.CharField(max_length=4, blank=True, null=True)
+    update_date = models.DateTimeField(blank=True, null=True)
 
