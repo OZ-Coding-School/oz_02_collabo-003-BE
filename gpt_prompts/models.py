9fe02f6d0eef8354148ff4e4eafc0c721440be54
BE-02-KMJ <mjk5912@naver.com>
2024-06-04 11:59:34 +0900

    Fix: DB gpt_prompts table admins_id -> user_id

--- gpt_prompts/models.py
+++ gpt_prompts/models.py
@@ -8,4 +8,4 @@ class GptPrompt(models.Model):
     status = models.CharField(max_length=30, blank=True, null=True)
     create_date = models.CharField(max_length=8, blank=True, null=True)
     last_date = models.CharField(max_length=8, blank=True, null=True)
-    admins_id = models.ForeignKey(kluck_Admin, on_delete=models.PROTECT, db_column='admins_id')
+    user_id = models.ForeignKey(kluck_Admin, on_delete=models.PROTECT, db_column='user_id')
