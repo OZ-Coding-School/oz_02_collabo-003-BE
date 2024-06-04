303b33bfd2da65f1aff1ff53e0982b71c131350b
BE-02-KMJ <mjk5912@naver.com>
2024-06-04 16:26:22 +0900

    Fix: admin 인증 관련 고난

--- gpt_prompts/serializers.py
+++ gpt_prompts/serializers.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
 from .models import GptPrompt
-from luck_messages.models import LuckMessage
 
 class PromptSerializer(serializers.ModelSerializer):
-    # 임의로 관리자 id가 1인 자료 생성.
-    admins_id = serializers.SerializerMethodField()
+    # # 임의로 관리자 id가 1인 자료 생성.
+    # admins_id = serializers.SerializerMethodField()
 
-    def get_admins_id(self, obj) -> int:
-        return 1
-
+    # def get_admins_id(self, obj) -> int:
+    #     return 1
     def create(self, validated_data):
         prompt_msg = validated_data.pop('prompt_msg')
         instance = GptPrompt.objects.create(prompt_msg=prompt_msg, **validated_data)
