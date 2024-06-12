5417fa72954e314f3e5d9ad744ededf2ad2fccff
Hyunminmax <hyunmin.shin81@gmail.com>
2024-06-12 16:03:40 +0900

    Conf:DB update

--- gpt_prompts/models.py
+++ gpt_prompts/models.py
@@ -3,9 +3,9 @@ from admins.models import *
 class GptPrompt(models.Model):
     gpt_id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=30, blank=True, null=True)
-    prompt_msg_name = models.CharField(max_length=100, blank=True, null=True)
     prompt_msg = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=30, blank=True, null=True)
     create_date = models.CharField(max_length=8, blank=True, null=True)
     last_date = models.CharField(max_length=8, blank=True, null=True)
     user_id = models.ForeignKey(kluck_Admin, on_delete=models.PROTECT, db_column='user_id')
+    # prompt_msg_name = models.CharField(max_length=100, blank=True, null=True) 사용안함
+    # status = models.CharField(max_length=30, blank=True, null=True) 사용안함
