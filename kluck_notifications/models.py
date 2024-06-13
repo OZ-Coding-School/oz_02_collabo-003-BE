52043b0284e143058e0f62dc9ff330bfbce9b136
joowhi <hwangjoowhi@gmail.com>
2024-06-13 13:22:47 +0900

    Feat: Add new model 'DeviceToken'

--- kluck_notifications/models.py
+++ kluck_notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class DeviceToken(models.Model):
+    token = models.CharField(max_length=255, unique=True, db_index=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.token
