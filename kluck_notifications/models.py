bf31ddb0a9e80eb7f8d4fcfa38150dcce2ba29ec
joowhi <hwangjoowhi@gmail.com>
2024-06-13 14:37:49 +0900

    Docs: Add comments to DeviceToken models.py

--- kluck_notifications/models.py
+++ kluck_notifications/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# 사용자 디바이스 토큰 저장
 class DeviceToken(models.Model):
     token = models.CharField(max_length=255, unique=True, db_index=True)
     create_date = models.DateTimeField(auto_now_add=True)
