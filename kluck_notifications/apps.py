8096e7d293c0400c61cfeaa73d10e277075cd3e7
joowhi <hwangjoowhi@gmail.com>
2024-06-14 15:00:17 +0900

    Chore: push branch

--- kluck_notifications/apps.py
+++ kluck_notifications/apps.py
@@ -16,25 +16,22 @@ class KluckNotificationsConfig(AppConfig):
         try:
             # AdminSetting 테이블에서 term_time 가져오기
             push_time = AdminSetting.objects.first().push_time
-            print(push_time, 'push_time!!!!!!!!')
             # 숫자 네자리를 문자열로 변환하여 분리
             push_time_str = str(push_time).zfill(4)  # 네자리로 맞추기 위해 zfill 사용
-            print(push_time_str, 'push_time_str!!!!!!!!')
             hour = int(push_time_str[:2])  # 앞 두 자리
-            print(hour, 'hour!!!!!!!!')
             minute = int(push_time_str[2:])  # 뒤 두 자리
-            print(minute, 'minute!!!!!!!!')
         except AttributeError:
             # 예외 처리: AdminSetting 객체가 없을 경우 기본값 설정
             hour = 8
             minute = 0
+
 
         push_scheduler = BackgroundScheduler(timezone=pytz.timezone('Asia/Seoul'))
         push_scheduler.add_job(
             send_push_notifications,
             trigger=CronTrigger(hour=hour, minute=minute),
         )
-        
+
         try:
             push_scheduler.start()
         except KeyboardInterrupt:
--- kluck_notifications/push_scheduler.py
+++ kluck_notifications/push_scheduler.py
@@ -1,6 +1,7 @@
 import firebase_admin # Firebase Admin SDK 사용
 from firebase_admin import credentials # 서비스 계정 키를 사용하여 Firebase Admin SDK 인증
 from firebase_admin import messaging # FCM 메시지 생성 및 전송
+from kluck_env import env_settings as env
 from datetime import datetime, timedelta
 from .models import DeviceToken
 from luck_messages.models import LuckMessage
@@ -10,12 +11,10 @@ def send_push_notifications():
     # firebase adminsdk 초기화
     cred_path = 'kluck_notifications/kluck-firebase.json'
     cred = credentials.Certificate(cred_path)
-    if not firebase_admin._apps:
-        firebase_admin.initialize_app(cred, {
+    firebase_admin.initialize_app(cred, {
             'projectId': 'k-luck',
         })
-    print("Firebase Admin SDK 초기화 완료")
-    
+
     try:
         # DB에서 디바이스 토큰 가져오기
         registration_tokens = list(DeviceToken.objects.values_list('token', flat=True))
@@ -24,13 +23,11 @@ def send_push_notifications():
         today = datetime.now().strftime("%Y%m%d")
         # DB에서 오늘의 운세 메시지 가져오기
         today_luck_msg = LuckMessage.objects.filter(luck_date=today, category='today').first()
-        print(f"오늘의 운세 메시지: {today_luck_msg}")
 
         # 오늘의 운세 메시지가 존재한다면 푸시 알림 보내기
         if today_luck_msg:
             title = '오늘의 운세'
             body = today_luck_msg.luck_msg
-            print(f"푸시 알림 내용: {body}")
             
             # 푸시 알림 메시지 생성
             message = messaging.MulticastMessage( # 여러 기기에 메시지 전송
@@ -57,11 +54,6 @@ def send_push_notifications():
 
             # Firebase로 푸시 알림 전송
             response = messaging.send_multicast(message)
-            print(f"{response.success_count} messages were sent successfully")
-            print(f"{response.failure_count} messages failed to send")
-            # # 알림 전송 실패 이유
-            # for i, resp in enumerate(response.responses):
-            #     if not resp.success:
-            #         print(f"Failed to send message to {registration_tokens[i]}: {resp.exception}")
+            
     except Exception as e:
         print(f"푸시 알림 전송 중 오류 발생: {e}")
